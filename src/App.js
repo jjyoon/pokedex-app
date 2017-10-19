504177026843ca48b5a31f946259c90c80a8992e
jjyoon <jim.yoon93@gmail.com>
2017-10-19 17:21:03 -0500

    Added sprites, simplified data fetching function

--- src/App.js
+++ src/App.js
@@ -20,29 +20,28 @@ class App extends Component {
 
   fetchPokemonData = async () => {
     try {
-      const { data } = await axios.get(
-        `https://cors.now.sh/https://pokeapi.co/api/v2/pokemon/${this.state
-          .pokemonIdName}`
-      )
-      this.setState({ pokemonData: data })
-      this.setState({ pokemonImg: data.sprites.front_default })
-      this.setState({ pokemonName: this.capitalize(data.name) })
-      console.log(this.state.pokemonData.sprites.front_default)
+      // const { data } = await axios.get(
+      //   `https://cors.now.sh/https://pokeapi.co/api/v2/pokemon/${this.state
+      //     .pokemonIdName}`
+      // )
+      // this.setState({ pokemonData: data })
+      // this.setState({ pokemonImg: data.sprites.front_default })
+      // this.setState({ pokemonName: this.capitalize(data.name) })
+      // console.log(this.state.pokemonData.sprites.front_default)
 
-      const data2 = await axios.get(
+      const { data } = await axios.get(
         `https://cors.now.sh/https://pokeapi.co/api/v2/pokemon-species/${this
           .state.pokemonIdName}`
       )
+      this.setState({ pokemonData: data })
+      this.setState({ pokemonName: data.names[0].name })
       this.setState({
-        pokemonDescription: data2.data.flavor_text_entries[1].flavor_text
+        pokemonDescription: data.flavor_text_entries[1].flavor_text
       })
     } catch (e) {
       console.log('the error was', e)
     }
   }
-
-  capitalize = str =>
-    str.length ? str[0].toUpperCase() + str.slice(1).toLowerCase() : ''
 
   handleSearchChange = event => {
     console.log(this.state.pokemonIdName)
@@ -79,7 +78,10 @@ class App extends Component {
           <p>{this.state.pokemonData.id}</p>
           <p>{this.state.pokemonName}</p>
           <p>{this.state.pokemonDescription}</p>
-          <img src={this.state.pokemonImg} alt={this.state.pokemonName} />
+          <img
+            src={`/assets/sprites/${this.state.pokemonData.id}.png`}
+            alt={this.state.pokemonName}
+          />
         </div>
       </div>
     )
