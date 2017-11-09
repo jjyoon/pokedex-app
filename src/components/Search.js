69d0183a9e94f0ffcc108362b77427635ac6ca6a
Jimmy Yoon <jim.yoon93@gmail.com>
2017-11-08 18:48:54 -0600

    Functioning search bar using React-Autocomplete component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
+import ReactAutocomplete from 'react-autocomplete'
+
+
 
 class Search extends Component {
+  state = {
+      value: '',
+  }
+
   render() {
     return (
-      <div>
-        <input
-          placeholder="Pokemon Name or Number"
-          value={this.props.value}
-          onChange={this.props.onChange}
-          onKeyPress={this.props.onKeyPress}
-        />
-        <button type="submit" onClick={this.props.onClick}>
-          Search
-        </button>
-      </div>
+      // <div>
+      //   <input
+      //     placeholder="Pokemon Name or Number"
+      //     value={this.props.value}
+      //     onChange={this.props.onChange}
+      //     onKeyPress={this.props.onKeyPress}
+      //   />
+      //   <button type="submit" onClick={this.props.onClick}>
+      //     Search
+      //   </button>
+      // </div>
+      <ReactAutocomplete
+        items={[
+          { id: 'foo', label: 'foo' },
+          { id: 'bar', label: 'bar' },
+          { id: 'baz', label: 'baz' },
+        ]}
+        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+        getItemValue={item => item.label}
+        renderItem={(item, highlighted) =>
+          <div
+            key={item.id}
+            style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+          >
+            {item.label}
+          </div>
+        }
+        value={this.state.value}
+        onChange={e => this.setState({ value: e.target.value })}
+        onSelect={value => this.setState({ value })}
+      />
     )
   }
 }
