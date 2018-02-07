d5d706ca790cca6229c77bfdcd6bf6db0d55e124
Jimmy Yoon <jim.yoon93@gmail.com>
2018-02-06 22:05:22 -0600

    Commit to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,140 +1,185 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import 'normalize.css'
-import axios from 'axios'
-import Search from './components/Search'
+import React, { Component } from "react";
+import blueLight from "./blueLight.svg";
+import "./App.css";
+import "normalize.css";
+import axios from "axios";
+import Search from "./components/Search";
+import Palette from "react-palette";
 
 class App extends Component {
-  state = {
-    pokemonData: [],
-    pokemonId: parseInt(window.location.pathname.slice(1)) || 150,
-    pokemonName: '',
-    pokemonIdAndName: '',
-    pokemonImg: '',
-    pokemonDescription: '',
-    loading: true
-  }
+	state = {
+		pokemonData: [],
+		pokemonId: parseInt(window.location.pathname.slice(1)) || 150,
+		pokemonName: "",
+		pokemonIdAndName: "",
+		pokemonImg: "",
+		pokemonDescription: "",
+		loading: true
+	};
 
-  componentDidMount() {
-    if (this.state.pokemonId) {
-      this.fetchPokemonData()
-    }
-  }
+	componentDidMount() {
+		if (this.state.pokemonId) {
+			this.fetchPokemonData();
+		}
+	}
 
-  fetchPokemonData = async () => {
-    try {
-      this.setState({ loading: true })
-      const pokemon = localStorage.getItem(`${this.state.pokemonId}`)
-      if (pokemon) {
-        var data = JSON.parse(pokemon)
-      } else {
-        var { data } = await axios.get(
-          `https://cors.now.sh/https://pokeapi.co/api/v2/pokemon-species/${
-            this.state.pokemonId
-          }`
-        )
-        localStorage.setItem(`${this.state.pokemonId}`, JSON.stringify(data))
-      }
+	fetchPokemonData = async () => {
+		try {
+			this.setState({ loading: true });
+			const pokemon = localStorage.getItem(`${this.state.pokemonId}`);
+			if (pokemon) {
+				var data = JSON.parse(pokemon);
+			} else {
+				var { data } = await axios.get(
+					`https://cors.now.sh/https://pokeapi.co/api/v2/pokemon-species/${
+						this.state.pokemonId
+					}`
+				);
+				localStorage.setItem(`${this.state.pokemonId}`, JSON.stringify(data));
+			}
 
-      await this.setState({ pokemonData: data })
-      // console.log(this.state.pokemonData);
-      await this.findEnglishVersion()
+			await this.setState({ pokemonData: data });
+			// console.log(this.state.pokemonData);
+			await this.findEnglishVersion();
 
-      await this.setState({
-        pokemonIdAndName: data.id + '. ' + this.state.pokemonName,
-        pokemonId: data.id
-      })
+			await this.setState({
+				pokemonIdAndName: data.id + ". " + this.state.pokemonName,
+				pokemonId: data.id
+			});
 
-      await window.history.pushState(null, null, this.state.pokemonId)
+			await window.history.pushState(null, null, this.state.pokemonId);
 
-      await this.setState({ loading: false })
-    } catch (e) {
-      console.log('the error was', e)
-    }
-  }
+			await this.setState({ loading: false });
 
-  findEnglishVersion = () => {
-    const dataArray = this.state.pokemonData
+			// const msg = new SpeechSynthesisUtterance(
+			// 	`${this.state.pokemonName}, ${this.state.pokemonDescription}`
+			// );
 
-    const englishName = dataArray.names.find(function(item, index) {
-      return item.language.name === 'en'
-    })
-    this.setState({ pokemonName: englishName.name })
+			// const voices = window.speechSynthesis.getVoices();
+			//
+			// msg.voice = voices[10]; // Note: some voices don't support altering params
+			// msg.voiceURI = "native";
+			// msg.volume = 1; // 0 to 1
+			// msg.rate = 1; // 0.1 to 10
+			// msg.pitch = 1; //0 to 2
 
-    const englishDescription = dataArray.flavor_text_entries.find(function(
-      item,
-      index
-    ) {
-      return item.language.name === 'en'
-    })
-    this.setState({ pokemonDescription: englishDescription.flavor_text })
-  }
+			// await window.speechSynthesis.speak(msg);
+		} catch (e) {
+			console.log("the error was", e);
+		}
+	};
 
-  handleSearchChange = event => {
-    console.log(this.state.pokemonIdAndName)
-    this.setState({ pokemonIdAndName: event.target.value })
-  }
+	findEnglishVersion = () => {
+		const dataArray = this.state.pokemonData;
 
-  handleSearch = () => {
-    this.fetchPokemonData()
-  }
+		const englishName = dataArray.names.find(function(item, index) {
+			return item.language.name === "en";
+		});
+		this.setState({ pokemonName: englishName.name });
 
-  handleEnterPress = e => {
-    if (e.key === 'Enter') {
-      this.handleSearch()
-    }
-  }
+		const englishDescription = dataArray.flavor_text_entries.find(function(
+			item,
+			index
+		) {
+			return item.language.name === "en";
+		});
+		this.setState({ pokemonDescription: englishDescription.flavor_text });
+	};
 
-  handleSelect = async (event, data) => {
-    console.log(data.id)
-    // await this.setState({ loading: true })
-    await this.setState({ pokemonIdAndName: data.label })
-    await this.setState({ pokemonId: data.id })
-    this.fetchPokemonData()
-  }
+	handleSearchChange = event => {
+		console.log(this.state.pokemonIdAndName);
+		this.setState({ pokemonIdAndName: event.target.value });
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Pokedex</h1>
-          <Search
-            value={this.state.pokemonIdAndName}
-            onChange={this.handleSearchChange}
-            onClick={this.handleSearch}
-            onKeyPress={this.handleEnterPress}
-            onSelect={this.handleSelect}
-          />
-        </header>
-        <div
-          className="c-pokemon-content-container"
-          loading={this.state.loading}
-        >
-          {this.state.loading ? (
-            <h1>Loading!</h1>
-          ) : (
-            <div className="pokemon-content">
-              <figure className="pokemon-image-container">
-                <img
-                  className="pokemon-image"
-                  src={`/assets/sprites/${this.state.pokemonData.id}.png`}
-                  alt={this.state.pokemonName}
-                />
-              </figure>
-              <div className="pokemon-content__text-description">
-                <p>{this.state.pokemonId}</p>
-                <p>{this.state.pokemonName}</p>
-                <p>{this.state.pokemonDescription}</p>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    )
-  }
+	handleSearch = () => {
+		this.fetchPokemonData();
+	};
+
+	handleEnterPress = e => {
+		if (e.key === "Enter") {
+			this.handleSearch();
+		}
+	};
+
+	handleSelect = async (event, data) => {
+		console.log(data.id);
+		// await this.setState({ loading: true })
+		await this.setState({ pokemonIdAndName: data.label });
+		await this.setState({ pokemonId: data.id });
+		this.fetchPokemonData();
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={blueLight} className="App-logo" alt="blue-light" />
+					<h1 className="App-title">Pokédex</h1>
+					<Search
+						value={this.state.pokemonIdAndName}
+						onChange={this.handleSearchChange}
+						onClick={this.handleSearch}
+						onKeyPress={this.handleEnterPress}
+						onSelect={this.handleSelect}
+					/>
+				</header>
+				<div
+					className="c-pokemon-content-container"
+					loading={this.state.loading}
+				>
+					{this.state.loading ? (
+						<h1 className="searching-title">Searching...</h1>
+					) : (
+						<div className="pokemon-content">
+							<figure className="pokemon-image-container">
+								<img
+									className="pokemon-image"
+									src={`/assets/sprites/${this.state.pokemonData.id}.png`}
+									alt={this.state.pokemonName}
+								/>
+								<Palette
+									image={`/assets/sprites/${this.state.pokemonData.id}.png`}
+								>
+									{palette => (
+										<div
+											style={{
+												backgroundColor: palette.lightVibrant,
+												borderColor: palette.lightMuted
+											}}
+											className="pokemon-image__background-color"
+										/>
+									)}
+								</Palette>
+							</figure>
+							<div className="pokemon-content__text-description">
+								<p className="pokemon-content__number">
+									{this.state.pokemonId}
+								</p>
+								<p className="pokemon-content__name">
+									{this.state.pokemonName}
+									<Palette
+										image={`/assets/sprites/${this.state.pokemonData.id}.png`}
+									>
+										{palette => (
+											<span
+												className="pokemon-name__background-color"
+												style={{
+													backgroundColor: palette.lightVibrant
+												}}
+											/>
+										)}
+									</Palette>
+								</p>
+								<p className="pokemon-content__description">
+									{this.state.pokemonDescription}
+								</p>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
-
-export default App
+export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,66 +1,111 @@
-import React, { Component } from 'react'
-import ReactAutocomplete from 'react-autocomplete'
+import React, { Component } from "react";
+import ReactAutocomplete from "react-autocomplete";
 // var EnglishPokemonList = require('./EnglishPokemonList').EnglishPokemonList
-import { EnglishPokemonList } from './EnglishPokemonList'
-import styled from 'styled-components'
+import { EnglishPokemonList } from "./EnglishPokemonList";
+import styled from "styled-components";
 
 class Search extends Component {
-  state = {
-    value: '',
-    pokemonList: EnglishPokemonList
-  }
+	state = {
+		value: "",
+		pokemonList: EnglishPokemonList
+	};
 
-  componentWillMount() {
-    var pokemonNameArray = this.state.pokemonList.map(function(item, index) {
-      index = index + 1
-      return { id: index, label: index + '. ' + item }
-    })
-    this.setState({ pokemonList: pokemonNameArray })
-  }
+	componentWillMount() {
+		var pokemonNameArray = this.state.pokemonList.map(function(item, index) {
+			index = index + 1;
+			return { id: index, label: index + ". " + item };
+		});
+		this.setState({ pokemonList: pokemonNameArray });
+	}
 
-  render() {
-    return (
-      <SearchBarContainer>
-        <ReactAutocomplete
-          items={this.state.pokemonList}
-          shouldItemRender={(item, value) =>
-            item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
-          }
-          getItemValue={item => item.label}
-          renderItem={(item, highlighted) => (
-            <div
-              key={item.id}
-              style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
-            >
-              {item.label}
-            </div>
-          )}
-          value={this.props.value}
-          onChange={this.props.onChange}
-          onSelect={this.props.onSelect}
-          onKeyPress={this.props.onKeyPress}
-        />
+	render() {
+		return (
+			<div className="c-search">
+				<ReactAutocomplete
+					items={this.state.pokemonList}
+					shouldItemRender={(item, value) =>
+						item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
+					}
+					getItemValue={item => item.label}
+					renderItem={(item, highlighted) => (
+						<div
+							key={item.id}
+							style={{
+								padding: "5px",
+								backgroundColor: highlighted ? "#ccc" : "transparent"
+							}}
+							onMouseEnter={() => {
+								console.log("hi");
+							}}
+						>
+							{item.label}
+						</div>
+					)}
+					value={this.props.value}
+					onChange={this.props.onChange}
+					onSelect={this.props.onSelect}
+					onKeyPress={this.props.onKeyPress}
+					menuStyle={{
+						color: "#000",
+						borderRadius: "3px",
+						boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+						background: "rgba(255, 255, 255, 0.9)",
+						fontSize: "90%",
+						top: "auto",
+						left: "auto",
+						marginTop: "5px",
+						textAlign: "left",
+						position: "fixed",
+						overflow: "auto",
+						maxHeight: "300px" // TODO: don't cheat, let it flow to the bottom
+					}}
+					inputProps={{
+						placeholder: "Pokemon Name/Number"
+					}}
+				/>
 
-        <SearchButton type="submit" onClick={this.props.onClick} className="c-search__button">
-          Go
-        </SearchButton>
-      </SearchBarContainer>
-    )
-  }
+				<button
+					type="submit"
+					onClick={this.props.onClick}
+					className="c-search__button"
+				>
+					Go
+				</button>
+			</div>
+		);
+	}
 }
 
-const SearchButton = styled.button`
-  background: green;
-  color: #fff;
-  border-radius: 10px;
-  margin-left: -40px;
-  overflow: hidden;
-`
+// const SearchButton = styled.button`
+// 	position: absolute;
+// 	top: 50%;
+// 	transform: translateY(-50%);
+// 	right: 5px;
+// 	height: 40px;
+// 	width: 70px;
+// 	background: #21d37a;
+// 	color: #fff;
+// 	border: none;
+// 	border-radius: 20px;
+// 	font-family: "Nunito", sans-serif;
+// 	text-transform: uppercase;
+// `;
 
-const SearchBarContainer = styled.div`
-  color: black;
-  overflow: hidden;
-  border-radius: 10px;
-`
+// const SearchBarContainer = styled.div`
+// 	position: relative;
+// 	color: black;
+// 	overflow: hidden;
+// 	text-align: left;
+//
+// 	input {
+// 		position: relative;
+// 		height: 50px;
+// 		width: 275px;
+// 		padding-left: 20px;
+// 		border: none;
+// 		border-radius: 25px;
+// 		font-family: "Nunito", sans-serif;
+// 	}
+// `;
 
-export default Search
+export default Search;
